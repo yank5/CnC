73ef50cc6d3faaf21c40b9dca6a1aeeeb8013d9c
Yank5 <Yank5@nycstudents.net>
2023-05-31 13:18:05 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.ExecutionException;
+
+public class Main {
+    public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException, ExecutionException {
+        HttpClient client=HttpClient.newHttpClient();
+        HttpRequest request= HttpRequest.newBuilder()
+                .uri(URI.create("https://raw.githubusercontent.com/yank5/requestip/main/README.md"))
+                .build();
+        var c=client.send(request, HttpResponse.BodyHandlers.ofString()).body();
+        System.out.println(c);
+
+        //System.out.println(c.complete());
+    }
+}
