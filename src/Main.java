08bb637dabce1b9c3f5ddb16d1fda62c7d0d87a9
Yank5 <Yank5@nycstudents.net>
2023-06-02 12:35:57 -0400

    1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.HttpClient;
@@ -7,13 +11,26 @@ import java.net.http.HttpResponse;
 import java.util.concurrent.ExecutionException;
 
 public class Main {
-    public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException, ExecutionException {
-        HttpClient client=HttpClient.newHttpClient();
-        HttpRequest request= HttpRequest.newBuilder()
-                .uri(URI.create("https://raw.githubusercontent.com/yank5/requestip/main/README.md"))
-                .build();
-        var c=client.send(request, HttpResponse.BodyHandlers.ofString()).body();
-        //System.out.println(c);
-        //System.out.println(c.complete());
+    public static void main(String[] args) {
+//        HttpClient client=HttpClient.newHttpClient();
+//        HttpRequest request= HttpRequest.newBuilder()
+//                .uri(URI.create("https://raw.githubusercontent.com/yank5/requestip/main/README.md"))
+//                .build();
+//        var c=client.send(request, HttpResponse.BodyHandlers.ofString()).body();
+//        System.out.println(c);
+        ServerSocket socket;
+        Socket socketaccept;
+        InputStream inputStream;
+        try {
+            socket = new ServerSocket(0);
+            socketaccept = socket.accept();
+            socketaccept.setKeepAlive(true);
+            inputStream = socketaccept.getInputStream();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        while(true){
+            //read is not complete
+        }
     }
 }
